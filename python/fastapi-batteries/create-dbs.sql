8abbe9aa625e845fc3b183dc072f3dd1b4681a18
Rami Maalouf <rami.maalouf@ibm.com>
2023-11-11 10:32:26 -0500

    added vector db to the python project

--- python/fastapi-batteries/create-dbs.sql
+++ python/fastapi-batteries/create-dbs.sql
@@ -1,2 +1,10 @@
 CREATE DATABASE db;
 CREATE DATABASE celery_schedule_jobs;
+CREATE EXTENSION IF NOT EXISTS vector;
+
+-- CREATE TABLE IF NOT EXISTS embeddings (
+--   id SERIAL PRIMARY KEY,
+--   embedding vector,
+--   text text,
+--   created_at timestamptz DEFAULT now()
+-- );
