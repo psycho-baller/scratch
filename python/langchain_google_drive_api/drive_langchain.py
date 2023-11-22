fdd9fbaf71d43f6ec4e9779c581afcc5b4545a39
Rami Maalouf <rami.maalouf@ibm.com>
2023-11-22 09:39:15 -0500

    using map reduce summary(my own template

--- python/langchain_google_drive_api/drive_langchain.py
+++ python/langchain_google_drive_api/drive_langchain.py
@@ -11,6 +11,8 @@ from googleapiclient.errors import HttpError
 from langchain.chains.summarize import load_summarize_chain
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import GoogleDriveLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.prompts import PromptTemplate
 
 # If modifying these SCOPES, delete your token.json file.
 SCOPES = ["https://www.googleapis.com/auth/drive.readonly"]
@@ -18,12 +20,49 @@ CLIENT_SECRET_FILE = 'token.json'
 CREDS_FILE = 'credentials.json'
 APPLICATION_NAME = 'Drive API Python Quickstart'
 
+llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k")
+
+def generate_actionable_takeaways(docs):
+    """Generates actionable takeaways from a list of documents
+    
+    Args:
+        docs (list): list of documents
+    Returns:
+        actionable_takeaways (list): list of actionable takeaways
+    """
+    
+    text_splitter = RecursiveCharacterTextSplitter(separators=["\n\n", "\n", "\r\n\r\n\r\n"], chunk_size=10000, chunk_overlap=200)
+    split_docs = text_splitter.split_documents(docs)
+    map_prompt = """
+Write a concise summary of the following:
+"{text}"
+CONCISE SUMMARY:
+"""
+    map_prompt_template = PromptTemplate(template=map_prompt, input_variables=["text"])
+    
+    combine_prompt = """
+Write a concise summary of the following text delimited by triple backquotes.
+Return your response in bullet points which covers the key points of the text.
+```{text}```
+BULLET POINT SUMMARY:
+"""
+    combine_prompt_template = PromptTemplate(template=combine_prompt, input_variables=["text"])
+
+    summary_chain = load_summarize_chain(llm=llm,
+                                     chain_type='map_reduce',
+                                     map_prompt=map_prompt_template,
+                                     combine_prompt=combine_prompt_template,
+#                                      verbose=True
+                                    )
+    output = summary_chain.run(docs)
+    return output
+
 def get_credentials():
+    """Gets valid user credentials from storage."""
     creds = None
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    """Gets valid user credentials from storage."""
     if os.path.exists("token.json"):
         creds = Credentials.from_authorized_user_file("token.json", SCOPES)
     # If there are no (valid) credentials available, let the user log in.
@@ -56,11 +95,12 @@ def main():
     # recursive=False,
 )
         docs = loader.load()
+        actionable_takeaways = generate_actionable_takeaways(docs)
+        print(f"Actionable takeaways: {actionable_takeaways}")
         
-        llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-16k")
-        chain = load_summarize_chain(llm, chain_type="stuff")
-        summary = chain.run(docs)
-        print(f"Summary of {docs}: {summary}")
+        # chain = load_summarize_chain(llm, chain_type="stuff")
+        # summary = chain.run(docs)
+        # print(f"Summary of {docs}: {summary}")
     except HttpError as e:
         print(e)
         
