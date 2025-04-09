2ef70ec9794ddd43830dd17ba114d011e8e5ab52
Rami Maalouf <rami.rami@ucalgary.ca>
2025-04-08 22:58:18 -0600

    generate token script

--- js/audio-pen/scripts/generate-token.js
+++ js/audio-pen/scripts/generate-token.js
@@ -1,13 +1,13 @@
 const jwt = require("jsonwebtoken");
 
 // Your Supabase JWT secret—replace this with your actual secret from Supabase dashboard
-const supabaseJwtSecret = "x3oq2DIQsFUhy/mvpZ9JQA0m8KWklpiGvmCZ/giMMU9Q0e7aSZjba9Cn16NUkDKvJmZNEO596TIJ0M0IPVf5og==";
+const supabaseJwtSecret = Deno.env.get("SUPABASE_JWT_SECRET");
 
 // Define your payload.
 // The 'sub' field is typically the user's ID, and 'exp' is the expiration time.
 // You can also include additional claims if needed.
 const payload = {
-  sub: "13f45d21-cbc2-4603-9142-b5b519df2b68", // The subject (typically a user ID)
+  sub: "<user_id>", // The subject (typically a user ID)
   // Set expiration to 1 hour (3600 seconds) from now
   exp: Math.floor(Date.now() / 1000) + 3600,
   // You can also add other custom claims here, for instance:
